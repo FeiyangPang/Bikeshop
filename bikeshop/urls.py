d8187171130813b1c66f821b166dbf4d0fd08cbd
FeiyangPang <pang_xw@126.com>
2025-10-20 00:15:53 +0800

    Update urls.py

--- bikeshop/urls.py
+++ bikeshop/urls.py
@@ -1,16 +1,21 @@
+# bikeshop/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as media_serve
 
-def healthz(_request): return HttpResponse("ok")
+def healthz(_): return HttpResponse("ok")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("shop.urls", "shop"), namespace="shop")),
     path("healthz", healthz),
+
+    # Serve user-uploaded files from the persistent disk, even with DEBUG=False
+    path("media/<path:path>", media_serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
-# serve uploaded media
+# (Keeping this as well is harmless; but the explicit route above guarantees it.)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
