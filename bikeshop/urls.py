dd4e3a80a13f66c10cd067133cfa9e40cea49035
FeiyangPang <pang_xw@126.com>
2025-10-19 22:35:54 +0800

    Refactor healthz function definition

--- bikeshop/urls.py
+++ bikeshop/urls.py
@@ -1,18 +1,16 @@
-# bikeshop/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
-def healthz(_request):
-    return HttpResponse("ok")
+def healthz(_request): return HttpResponse("ok")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("shop.urls", "shop"), namespace="shop")),
-    path("healthz", healthz),   # Render health check
+    path("healthz", healthz),
 ]
 
-# Serve uploaded media (fine for small sites behind gunicorn)
+# serve uploaded media
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
