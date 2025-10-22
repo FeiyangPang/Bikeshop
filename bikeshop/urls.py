083f7108b7172c521eced63817506a5ec7d7672b
FeiyangPang <pang_xw@126.com>
2025-10-22 14:54:43 +0800

    Update urls.py

--- bikeshop/urls.py
+++ bikeshop/urls.py
@@ -1,13 +1,18 @@
 # bikeshop/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve as media_serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("shop.urls", "shop"), namespace="shop")),
 ]
 
-# Serve uploaded files from MEDIA_ROOT at /media/
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded files from MEDIA_ROOT at /media/ even when DEBUG=False
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", media_serve, {
+        "document_root": settings.MEDIA_ROOT,
+        "show_indexes": False,
+    }),
+]
